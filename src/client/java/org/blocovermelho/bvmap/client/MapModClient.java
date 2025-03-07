7004ac8cd472615d7e90e590548f7f55232f77de
alikindsys <alice@blocovermelho.org>
2025-03-07 15:52:32 -0300

    Initial Commit
    This has nothing but a working split-set 1.21.4 fabric modding development environment.

---
+++ src/client/java/org/blocovermelho/bvmap/client/MapModClient.java
@@ -0,0 +1,10 @@
+package org.blocovermelho.bvmap.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MapModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/blocovermelho/bvmap/MapMod.java
@@ -0,0 +1,10 @@
+package org.blocovermelho.bvmap;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MapMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
