6c4489be5c0ca02808a9121265a5b216d29f809d
alikindsys <alice@blocovermelho.org>
2025-03-08 20:07:25 -0300

    Add ScrappyFiddleScreen.java

---
+++ src/client/java/org/blocovermelho/bvmap/client/screen/ScrappyFiddleScreen.java
@@ -0,0 +1,18 @@
+package org.blocovermelho.bvmap.client.screen;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+/**
+ * This is no place of honor
+ * No highly esteemed deed is commemorated here
+ * This is the scrappy fiddle:tm:
+ * It is temporary, broken and filled with bad code
+ * The importance of this is to get things going and documented
+ * and for us to not copy code without understanding what it does.
+ */
+public class ScrappyFiddleScreen extends Screen {
+    protected ScrappyFiddleScreen(Text title) {
+        super(title);
+    }
+}
