36228191b67108e61ca13cabcc67937bb1cca023
alikindsys <alice@blocovermelho.org>
2025-03-08 21:07:15 -0300

    Add helper for generating namespaced ids

--- src/main/java/org/blocovermelho/bvmap/MapMod.java
+++ src/main/java/org/blocovermelho/bvmap/MapMod.java
@@ -1,13 +1,19 @@
 package org.blocovermelho.bvmap;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MapMod implements ModInitializer {
     public static final String ID = "bvmod";
     public static Logger LOGGER = LoggerFactory.getLogger(ID);
+
     @Override
     public void onInitialize() {
+    }
+
+    public static Identifier id(String path) {
+        return Identifier.of(ID, path);
     }
 }
