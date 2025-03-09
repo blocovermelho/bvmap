eac35fdf6a59b55b5dbaf672670140d7f9bd5dee
alikindsys <alice@blocovermelho.org>
2025-03-08 21:02:15 -0300

    Add Logger

--- src/main/java/org/blocovermelho/bvmap/MapMod.java
+++ src/main/java/org/blocovermelho/bvmap/MapMod.java
@@ -1,9 +1,12 @@
 package org.blocovermelho.bvmap;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MapMod implements ModInitializer {
-
+    public static final String ID = "bvmod";
+    public static Logger LOGGER = LoggerFactory.getLogger(ID);
     @Override
     public void onInitialize() {
     }
