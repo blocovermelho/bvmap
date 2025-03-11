fd984258f29d453fd0ad9bc7122eb0995e3f0153
alikindsys <alice@blocovermelho.org>
2025-03-11 14:31:55 -0300

    Store explored regions

--- src/client/java/org/blocovermelho/bvmap/client/MapModClient.java
+++ src/client/java/org/blocovermelho/bvmap/client/MapModClient.java
@@ -1,10 +1,14 @@
 package org.blocovermelho.bvmap.client;
 
 import folk.sisby.surveyor.WorldSummary;
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.util.math.ChunkPos;
 
 public class MapModClient implements ClientModInitializer {
+    public static ObjectOpenHashSet<ChunkPos> EXPLORED_REGIONS = new ObjectOpenHashSet<>();
+
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(new Keybind());
